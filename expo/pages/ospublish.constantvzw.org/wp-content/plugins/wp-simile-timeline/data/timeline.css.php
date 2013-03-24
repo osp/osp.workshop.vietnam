0fedc4e0976d01eb20708d7dbb39170a17da53de
Stéphanie Vilayphiou <stephanie@stdin.fr>
2013-03-24 14:54:39 +0100

    Well, I think it's time that we put those files on the server...

---
+++ expo/pages/ospublish.constantvzw.org/wp-content/plugins/wp-simile-timeline/data/timeline.css.php
@@ -0,0 +1,21 @@
+
+/* Dynamic theme used by the plugin (attributes set in options panel)
+=====================================================================*/
+
+.dynamic-theme { color: #eee; }
+.dynamic-theme .timeline-band-0 .timeline-ether-bg { background-color: #eee; }
+.dynamic-theme .timeline-band-0 .timeline-ether-highlight { background-color: #aaa; }
+.dynamic-theme .timeline-band-0 .timeline-date-label-em { color: #aaa;  }
+.dynamic-theme .timeline-band-0 .timeline-date-label { color: #aaa; border-color: #aaa;  }
+.dynamic-theme .timeline-band-0 .timeline-event-label { color: #aaa; }
+.dynamic-theme .timeline-band-0 .timeline-ether-lines { border-color: #aaa; border-style: solid; }
+.dynamic-theme .timeline-band-0 .timeline-highlight-label-start{ color: #444; }
+.dynamic-theme .timeline-band-0 .timeline-highlight-label-end{ color: #444; }
+.dynamic-theme .timeline-band-1 .timeline-ether-bg { background-color: #ddd; }
+.dynamic-theme .timeline-band-1 .timeline-ether-highlight { background-color: #aaa; }
+.dynamic-theme .timeline-band-1 .timeline-date-label-em { color: #aaa;  }
+.dynamic-theme .timeline-band-1 .timeline-date-label { color: #aaa; border-color: #aaa;  }
+.dynamic-theme .timeline-band-1 .timeline-event-label { color: #aaa; }
+.dynamic-theme .timeline-band-1 .timeline-ether-lines { border-color: #aaa; border-style: solid; }
+.dynamic-theme .timeline-band-1 .timeline-highlight-label-start{ color: #444; }
+.dynamic-theme .timeline-band-1 .timeline-highlight-label-end{ color: #444; }
